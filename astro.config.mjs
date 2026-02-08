ad0230011de514816fa11dbe995c242a6351f564
gradykorchinski-ship-it <grady.korchinski@gmail.com>
2026-02-07 19:25:29 -0600

    Possibly fixed css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
     output: 'static',
-    adapter: node({ mode: 'standalone' }),
     integrations: [tailwind()],
 });
